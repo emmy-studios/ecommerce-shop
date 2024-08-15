910d4044cd5d0f76e2d50d356e9f5971cadf6c07
emmy-studios <emmystudios@proton.me>
2024-08-15 10:55:00 -0600

    "Add language dropdown"

--- resources/views/components/partials/navigation.blade.php
+++ resources/views/components/partials/navigation.blade.php
@@ -1,4 +1,4 @@
-<header x-data="{ responsiveNav: false }">
+<header x-data="{ responsiveNav: false, languageDropdown: false }">
 
     <nav class="border-b px-4 py-2 bg-white relative z-50">
 
@@ -26,7 +26,7 @@
  
                     {{-- Language Switcher --}}
                     <button @click="languageSwitch = !languageSwitch">
-                        <li><a href="#">Language</a></li>
+                        <li><a href="#">{{ __('Language') }}</a></li>
                     </button>
                     <livewire:dropdowns.language-switcher />
 
@@ -77,6 +77,24 @@
 
         <div class="flex flex-row px-4 py-2 bg-purple-600 text-white hover:bg-purple-500 hover:text-white">
             <a href="{{ route('brands') }}">{{ __('Brands') }}</a>
+        </div>
+
+        {{-- Language Dropdown --}}
+        <div class="flex flex-col">
+            <div class="flex flex-row px-4 py-2 bg-purple-600 text-white hover:bg-white hover:text-purple-500">
+                <button @click="languageDropdown = !languageDropdown" class="flex items-center">
+                    <a>{{ __('Language') }}</a>
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 ml-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
+                    </svg>
+                </button>
+            </div>
+            
+            {{-- Language Options --}}
+            <div x-show="languageDropdown" x-cloak class="flex flex-col bg-purple-200 text-purple-600 pl-8">
+                <a href="{{ route(Route::currentRouteName(), ['locale' => 'es']) }}" class="py-2 hover:bg-purple-500 hover:text-white">Español</a>
+                <a href="{{ route(Route::currentRouteName(), ['locale' => 'en']) }}" class="py-2 hover:bg-purple-500 hover:text-white">English</a>
+            </div>
         </div>
 
     </div>
--- resources/views/livewire/dropdowns/language-switcher.blade.php
+++ resources/views/livewire/dropdowns/language-switcher.blade.php
@@ -5,19 +5,41 @@
     class="absolute right-0 mt-10 py-2 w-32 bg-white rounded-lg shadow-xl"
     >
 
-    <a 
-        class="block px-2 py-2 text-gray-500 hover:bg-purple-500 hover:text-white" 
-        href="{{ url('es/' . implode('/', array_slice(request()->segments(), 1))) }}"
-        >
-        es
-    </a>
+    <div 
+        class="grid grid-cols-2 items-center justify-center hover:bg-purple-500">
+        <div>
+            <a 
+                class="block px-2 py-2 text-gray-500 hover:text-white" 
+                href="{{ url('en/' . implode('/', array_slice(request()->segments(), 1))) }}"
+                >
+                en
+            </a>
+        </div>
+        <div class="flex flex-row justify-center">
+            <img
+                class="w-8" 
+                src="{{ asset('assets/images/flags/us-flag.png') }}" 
+                alt="Unitated States Flag">
+        </div>
+    </div>
 
-    <a 
-        class="block px-2 py-2 text-gray-500 hover:bg-purple-500 hover:text-white" 
-        href="{{ url('en/' . implode('/', array_slice(request()->segments(), 1))) }}"
-        >
-        en
-    </a>
+    <div 
+        class="grid grid-cols-2 items-center justify-center hover:bg-purple-500">
+        <div>
+            <a 
+                class="block px-2 py-2 text-gray-500 hover:text-white" 
+                href="{{ url('es/' . implode('/', array_slice(request()->segments(), 1))) }}"
+                >
+                es
+            </a>
+        </div>
+        <div class="flex flex-row justify-center">
+            <img
+                class="w-8" 
+                src="{{ asset('assets/images/flags/spain-flag.png') }}" 
+                alt="Spain Flag">
+        </div>
+    </div>
 
 </div>
 
